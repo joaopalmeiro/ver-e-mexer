c0e25b7f75c425eca9dccaf48f75f7e43859d57d
João Palmeiro <joaommpalmeiro@gmail.com>
2021-01-18 02:23:50 +0000

    Update metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "ver-e-mexer",
+    title: "Ver e Mexer",
     htmlAttrs: {
       lang: "en"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
